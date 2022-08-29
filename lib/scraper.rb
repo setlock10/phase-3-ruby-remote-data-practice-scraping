5ab28f1e0953649e429880054bddb105349de874
John J Setlock II <johnsetlock@gmail.com>
2022-08-29 12:30:55 -0500

    Completed Assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# puts doc
+
+
+# page = Nokogiri::HTML(open("https://www.netflix.com/browse",:http_basic_authentication => [johnsetlock@gmail.com,10Bobcat]))
+
+binding.pry
